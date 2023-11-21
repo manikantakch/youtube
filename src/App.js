e1aa6022817023c85b991cd6405f12eaacf5411b
Manikanta_ch_k <manikanta.k@narayanagroup.com>
2023-11-21 09:08:29 +0530

    Commented code

--- src/App.js
+++ src/App.js
@@ -1,101 +1,102 @@
 import React from 'react';
 import Tabs from './components/Tabs';
 
-const FirstTab = () => {
-  return <div>first tab</div>
-}
+// const FirstTab = () => {
+//   return <div>first tab</div>
+// }
 
-const SecondTab = () => {
-  return <div>second tab</div>
-}
+// const SecondTab = () => {
+//   return <div>second tab</div>
+// }
 
-const ThridTab = () => {
-  return <div>Third tab</div>
-}
+// const ThridTab = () => {
+//   return <div>Third tab</div>
+// }
 
 
 function App() {
 
-  const tabList = [
-    {
-      label:'Tab-1',
-      Component:<FirstTab />
-    },
-    {
-      label:'Tab-2',
-      Component:<SecondTab />
-    },
-    {
-      label:'Tab-3',
-      Component:<ThridTab />
+  // const tabList = [
+  //   {
+  //     label:'Tab-1',
+  //     Component:<FirstTab />
+  //   },
+  //   {
+  //     label:'Tab-2',
+  //     Component:<SecondTab />
+  //   },
+  //   {
+  //     label:'Tab-3',
+  //     Component:<ThridTab />
 
-    },
-    {
-      label:'Tab-4',
-      Component:<FirstTab />
-    },
-    {
-      label:'Tab-5',
-      Component:<SecondTab />
-    },
-    {
-      label:'Tab-6',
-      Component:<ThridTab />
+  //   },
+  //   {
+  //     label:'Tab-4',
+  //     Component:<FirstTab />
+  //   },
+  //   {
+  //     label:'Tab-5',
+  //     Component:<SecondTab />
+  //   },
+  //   {
+  //     label:'Tab-6',
+  //     Component:<ThridTab />
 
-    },
-    {
-      label:'Tab-1',
-      Component:<FirstTab />
-    },
-    {
-      label:'Tab-2',
-      Component:<SecondTab />
-    },
-    {
-      label:'Tab-3',
-      Component:<ThridTab />
+  //   },
+  //   {
+  //     label:'Tab-1',
+  //     Component:<FirstTab />
+  //   },
+  //   {
+  //     label:'Tab-2',
+  //     Component:<SecondTab />
+  //   },
+  //   {
+  //     label:'Tab-3',
+  //     Component:<ThridTab />
 
-    },
-    // {
-    //   label:'Tab-4',
-    //   Component:<FirstTab />
-    // },
-    // {
-    //   label:'Tab-5',
-    //   Component:<SecondTab />
-    // },
-    // {
-    //   label:'Tab-6',
-    //   Component:<ThridTab />
+  //   },
+  //   {
+  //     label:'Tab-4',
+  //     Component:<FirstTab />
+  //   },
+  //   {
+  //     label:'Tab-5',
+  //     Component:<SecondTab />
+  //   },
+  //   {
+  //     label:'Tab-6',
+  //     Component:<ThridTab />
 
-    // },
-    // {
-    //   label:'Tab-1',
-    //   Component:<FirstTab />
-    // },
-    // {
-    //   label:'Tab-2',
-    //   Component:<SecondTab />
-    // },
-    // {
-    //   label:'Tab-3',
-    //   Component:<ThridTab />
+  //   },
+  //   {
+  //     label:'Tab-1',
+  //     Component:<FirstTab />
+  //   },
+  //   {
+  //     label:'Tab-2',
+  //     Component:<SecondTab />
+  //   },
+  //   {
+  //     label:'Tab-3',
+  //     Component:<ThridTab />
 
-    // },
-    // {
-    //   label:'Tab-4',
-    //   Component:<FirstTab />
-    // },
-    // {
-    //   label:'Tab-5',
-    //   Component:<SecondTab />
-    // },
-    // {
-    //   label:'Tab-6',
-    //   Component:<ThridTab />
+  //   },
+  //   {
+  //     label:'Tab-4',
+  //     Component:<FirstTab />
+  //   },
+  //   {
+  //     label:'Tab-5',
+  //     Component:<SecondTab />
+  //   },
+  //   {
+  //     label:'Tab-6',
+  //     Component:<ThridTab />
 
-    // }
-  ]
+  //   }
+  // ]
+  // <Tabs tabList={tabList}/> 
 
   return (
     <div >
@@ -107,7 +108,7 @@ function App() {
 
 
 
-   <Tabs tabList={tabList}/> 
+
     </div>
   );
 }
